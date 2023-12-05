4bc316fad51c2d2387e42f1469899a0ec10d962e
jultty <GIT_EMAIL>
2023-12-05 19:21:38 -0300

    Scaffold http4s

--- app/src/main/scala/Main.scala
+++ app/src/main/scala/Main.scala
@@ -1,2 +1,11 @@
+import cats.effect._
+import org.http4s._
+import org.http4s.dsl.io._
+
+val hello = HttpRoutes.of[IO] {
+  case GET -> Root / "hello" / name =>
+    Ok(s"Hello, $name.")
+}
+
 @main def start =
   print_environment_properties
