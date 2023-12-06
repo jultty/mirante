5dd5444790cee014a2aaf51a5c7f6430feb72795
jultty <GIT_EMAIL>
2023-12-05 22:12:17 -0300

    Setup case class JSON encoding

--- app/src/main/scala/Main.scala
+++ app/src/main/scala/Main.scala
@@ -6,17 +6,24 @@ import org.http4s.implicits._
 import org.http4s.circe._
 import org.http4s.ember.server._
 import io.circe._
+import io.circe.generic.auto._
+import io.circe.syntax._
 import io.circe.literal._
 import com.comcast.ip4s._
 
 def hello_json(name: String): Json =
   json"""{"hello": $name}"""
 
+case class Hello(name: String)
+case class User(name: String)
+
 object Main extends IOApp {
   val hello = HttpRoutes.of[IO] {
     case GET -> Root / "hello" / name =>
       Ok(hello_json(name))
-}.orNotFound
+    case GET -> Root / "user" / name =>
+      Ok(User(name).asJson)
+  }.orNotFound
 
   def run(args: List[String]): IO[ExitCode] =
     print_environment_properties
