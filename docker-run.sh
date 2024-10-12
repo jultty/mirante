7f131d31e6f8f686b4038df4c9bb2bd7e3ff7db6
jultty <juno.t@tuta.io>
2024-10-12 01:23:10 -0300

    Scaffold creation scripts and an example configuration

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env sh
+
+# password provided here is not used later
 docker run \
   -d \
-  --name mirante-pgr \
+  --name mirante-pg \
   -e POSTGRES_PASSWORD="$(gpg --gen-random 30)" \
   -p 5432:5432 \
   postgres:17.0-alpine3.20 \
