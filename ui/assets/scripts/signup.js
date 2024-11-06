942bb73c7cf44c6e1cd42896d00f0d8abfd8e3f3
jultty <jutty@tuta.io>
2024-11-06 08:20:42 -0300

    Add signup error messages to demo UI

--- ui/assets/scripts/signup.js
+++ ui/assets/scripts/signup.js
@@ -1,7 +1,9 @@
 const form = document.getElementById('account_creation_form')
+const dialog = document.getElementById('user_dialog')
 
 async function sign_up_handler(event) {
   event.preventDefault();
+  dialog.innerText = ''
 
   const form_data = Object.fromEntries(new FormData(form));
 
@@ -11,12 +13,23 @@ async function sign_up_handler(event) {
     body: JSON.stringify(form_data),
   })
 
+  console.log(response)
+
   response_json = await response.json()
 
-  credentials = {
-    "email": response_json["user"].email,
-    "token": response_json.token,
+  let credentials
+
+  if (response.status == 200 || response.status == 201) {
+    credentials = {
+      "email": response_json["user"].email,
+      "token": response_json.token,
+    }
+    dialog.innerText = 'Conta criada com sucesso'
   }
+  else if (response.status == 409)
+    dialog.innerText = 'Uma conta com este email já existe'
+  else
+    dialog.innerText = `Erro ${response.status} ao criar conta`
 
   sessionStorage.setItem("mirante_credentials", JSON.stringify(credentials))
   let stored_credentials = JSON.parse(sessionStorage.getItem("mirante_credentials"))
